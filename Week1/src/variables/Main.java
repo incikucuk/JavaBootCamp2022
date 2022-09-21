9b41359fdbbab46de2ace19b739a029da9851170
İnciKüçük <56034341+incikucuk@users.noreply.github.com>
2022-09-21 12:44:26 +0300

    Commit variables

---
+++ Week1/src/variables/Main.java
@@ -0,0 +1,13 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Ogrenci sayisi : ";
+		
+		System.out.println("Ogrenci Sayisi : "+ ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}
